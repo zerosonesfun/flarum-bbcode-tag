41fb53df29cf5de88c2df593dabae377af19fcc8
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2021-04-04 06:22:36 -0400

    Update extend.php

--- extend.php
+++ extend.php
@@ -16,10 +16,22 @@ return [
                 '[t]{TEXT100}[/t]',
                 '<a href="/all?q={TEXT100}" class="tagPhrase" title="Search for {TEXT100}">{TEXT100}</a>'
             );
-
             $config->BBCodes->addCustom(
                 '[ht={TEXT101}]',
-                '<a href="/all?q={TEXT101}" class="tagPhrase" title="Search for {TEXT101}">#{TEXT101}</a>'
+                '<a href="/all?q={TEXT101}" class="tagPhrase-Hashtag" title="Search for {TEXT101}">#{TEXT101}</a>'
             );
+            $config->BBCodes->addCustom(
+                '[ct={TEXT102}]',
+                '<a href="https://www.google.com/search?q={TEXT102}+stock" class="tagPhrase-Stock" title="Check on stock {TEXT102}">${TEXT102}</a>'
+            );
+            $config->BBCodes->addCustom(
+                '[g={TEXT103}]',
+                '<a href="https://www.google.com/search?q={TEXT103}" class="tagPhrase-Google" title="Google {TEXT103}">{TEXT103}</a>'
+            );
+            $config->BBCodes->addCustom(
+                '[ts={TEXT104}]',
+                '<a href="https://twitter.com/search/?q={TEXT104}" class="tagPhrase-Twitter" title="Twitter search for {TEXT104}">{TEXT104}</a>'
+            );
+
         })
 ];
